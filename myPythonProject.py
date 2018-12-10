f948e5e3a3ad9427802848076c59ef94d7d03d6b
longsjj <rongjunzeng@gmail.com>
2018-12-10 19:09:49 +0800

    modify

--- myPythonProject.py
+++ myPythonProject.py
@@ -9,10 +9,17 @@ class Solution(object):
             return arr[0]
 
         return arr[0]+self.solution(arr[1:])
+    def solution_sm(self,arr):
+        sum =i= 0
+        while i<len(arr):
+            sum+=arr[i]
+            i+=1
+        return sum
 
 
 if __name__ == '__main__':
 
     sl = Solution()
     v = sl.solution([1,2,3,4,5])
-    print(v)
+    v1 = sl.solution_sm([1,2,3,4,5])
+    print(v,v1)
