584632677060094eb2a9101a741b1720a7be705a
longsjj <rongjunzeng@gmail.com>
2018-12-10 19:36:04 +0800

    add git_common_code and some others

--- myPythonProject.py
+++ myPythonProject.py
@@ -15,11 +15,16 @@ class Solution(object):
             sum+=arr[i]
             i+=1
         return sum
-
+    def solution_sm_std(self,arr):
+        sum = 0
+        for v in arr:
+            sum+=v
+        return sum
 
 if __name__ == '__main__':
 
     sl = Solution()
     v = sl.solution([1,2,3,4,5])
     v1 = sl.solution_sm([1,2,3,4,5])
-    print(v,v1)
+    v2 = sl.solution_sm_std([1,2,3,4,5])
+    print(v,v1,v2)
